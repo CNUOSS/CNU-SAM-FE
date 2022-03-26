5432e3bd082e918320a564b95f410ed1b694d622
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-03-26 19:54:15 +0900

    [feat] install emotionJS for styling
    - install emotionJS for styling
    - implement GlobalStyle.tsx for global styling

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+function GlobalStyle(): React.ReactElement {
+  return (
+    <Global
+      styles={css`
+        :root {
+          --vh: 100%;
+        }
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        html,
+        body {
+          font-size: 62.5%;
+        }
+        body {
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+        button {
+          border: none;
+          outline: none;
+          cursor: pointer;
+        }
+        ul {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;

9e71d627c9dd308ac27d4b09d764a6e19a8ad587
parkeunsik <qkrdmstlr3@naver.com>
2022-04-04 19:41:08 +0900

    [chore]  set themeProvider of emotionJS
    - using ThemeProvider of "@emotion/react"
      - set storybook webpack file
      - set storybook preview
      - declare Theme type
      - create theme file

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@emotion/react';
+import GlobalStyle from './style/GlobalStyle';
 import App from './App';
 import Helmet from './libs/Helmet';
+import { theme } from './style/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet />
-    <App />
+    <ThemeProvider theme={theme}>
+      <Helmet />
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

02f7f0281069bb2346c3eec28c08d101c04df2ee
parkeunsik <qkrdmstlr3@naver.com>
2022-04-04 12:55:13 +0900

    [chore] change react-helmet name and route
    - change folder name from lib to libs
    - change component name from react-helmet.tsx to Helmet.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Helmet from './libs/Helmet';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

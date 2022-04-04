35bdefa048ac91025bca55dd2541a566bbb2f05f
parkeunsik <qkrdmstlr3@naver.com>
2022-04-04 14:51:33 +0900

    [chore] init storybook
    - initialize storybook for UI testing

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+  ],
+  core: {
+    builder: 'webpack5',
+  },
+  typescript: { reactDocgen: false },
+};

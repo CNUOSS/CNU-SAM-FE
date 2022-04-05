6f9c1c35af9bcb1284ad747c13af86813d5e0ca0
parkeunsik <qkrdmstlr3@naver.com>
2022-04-05 11:01:53 +0900

    [feat] apply i18n to Sidebar Container
    - To support english language
      - changeLanguage function of i18n is used
      - change language constant format

--- src/components/containers/Sidebar/index.tsx
+++ src/components/containers/Sidebar/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import i18n from 'i18next';
+import { useTranslation } from 'react-i18next';
 import * as Style from './styled';
 import Dropdown from '../../widgets/Dropdown';
 import logoImage from '../../../assets/images/logo.jpg';
@@ -9,21 +11,28 @@ interface SidebarProps {
 }
 
 function Sidebar({ isLogin }: SidebarProps) {
+  const { t } = useTranslation();
+  const languageNames = LANGUAGES.map((l) => l.name);
+
+  const changeLanguage = (selectedIdx: number) => {
+    i18n.changeLanguage(LANGUAGES[selectedIdx].lang);
+  };
+
   return (
     <Style.Container>
       <Style.Top isLogin={isLogin}>
         <Style.Logo src={logoImage} alt="logo" />
-        <Style.Title>소프트웨어 자산관리프로그램</Style.Title>
+        <Style.Title>{t('page:title')}</Style.Title>
         <Style.Version>v1.0.0</Style.Version>
-        <Dropdown items={LANGUAGES} onClickItem={() => {}} />
+        <Dropdown items={languageNames} onClickItem={changeLanguage} />
       </Style.Top>
       {/* TODO: login or userInfo */}
       {isLogin && (
         <Style.MenuList>
-          <Style.MenuItem>라이센스 목록</Style.MenuItem>
-          <Style.MenuItem>소프트웨어 목록</Style.MenuItem>
-          <Style.MenuItem>프로젝트 목록</Style.MenuItem>
-          <Style.MenuItem>그룹 목록</Style.MenuItem>
+          <Style.MenuItem>{t('page:licenseList')}</Style.MenuItem>
+          <Style.MenuItem>{t('page:softwareList')}</Style.MenuItem>
+          <Style.MenuItem>{t('page:projectList')}</Style.MenuItem>
+          <Style.MenuItem>{t('page:groupList')}</Style.MenuItem>
         </Style.MenuList>
       )}
     </Style.Container>

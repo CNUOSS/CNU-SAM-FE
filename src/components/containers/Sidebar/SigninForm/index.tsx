2d5cb6807d4a239be90fad275dd69d5d1667ee71
parkeunsik <qkrdmstlr3@naver.com>
2022-04-05 16:02:30 +0900

    [feat] add i18n to SigninForm
    - add i18n to SigninForm for english support

--- src/components/containers/Sidebar/SigninForm/index.tsx
+++ src/components/containers/Sidebar/SigninForm/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import * as Style from './styled';
 
 function SigninForm() {
+  const { t } = useTranslation();
+
   return (
     <Style.Container>
       <Style.SiginWrapper>
@@ -9,9 +12,9 @@ function SigninForm() {
           <Style.Input />
           <Style.Input />
         </Style.InputWrapper>
-        <Style.SigninButton>로그인</Style.SigninButton>
+        <Style.SigninButton>{t('page:signin')}</Style.SigninButton>
       </Style.SiginWrapper>
-      <Style.SignupButton>회원가입</Style.SignupButton>
+      <Style.SignupButton>{t('page:signup')}</Style.SignupButton>
     </Style.Container>
   );
 }

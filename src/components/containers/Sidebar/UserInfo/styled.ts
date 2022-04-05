3a10c998b9f655968729e428ba6f924b50390c11
parkeunsik <qkrdmstlr3@naver.com>
2022-04-05 15:58:12 +0900

    [feat] implement UserInfo Component
    - if login state, implement UserInfo Component
    - apply translation

---
+++ src/components/containers/Sidebar/UserInfo/styled.ts
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const InfoWrapper = styled.div`
+  font-size: 1.8rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const IconWrapper = styled.div`
+  width: 3rem;
+`;
+
+export const Name = styled.span`
+  font-weight: bold;
+`;
+
+export const Logout = styled.span`
+  padding-left: 1rem;
+  border-left: 1px solid black;
+  cursor: pointer;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: 2.5rem;
+`;
+
+export const EnrollButton = styled.div`
+  width: 100%;
+  height: 4.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0.5rem 0;
+
+  color: white;
+  border: none;
+  border-radius: 1rem;
+  font-size: 1.8rem;
+  font-weight: bold;
+  background-color: ${(props) => props.theme.colors.secondary};
+  cursor: pointer;
+`;

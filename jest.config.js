e711b010fb768468bbd35276052079c579a9bf91
parkeunsik <qkrdmstlr3@naver.com>
2022-04-07 12:54:42 +0900

    [test] make rtl libs file
    - To support Providers for unit test using rtl, make rtl util file

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: ['**/src/**/*.test.*'],
 };

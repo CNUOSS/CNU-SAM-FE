790f25e4b32e4a2f630ef7d8c79463ecd4f55e99
parkeunsik <qkrdmstlr3@naver.com>
2022-04-07 22:45:15 +0900

    [test] implement Sidebar Container test code

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   coverageDirectory: 'jest-coverage',
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js',
+  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testMatch: ['**/src/**/*.test.*'],
 };

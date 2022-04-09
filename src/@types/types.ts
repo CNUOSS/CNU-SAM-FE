a22c1a3cc539b905879e88bd8fafa0f34775f14c
parkeunsik <qkrdmstlr3@naver.com>
2022-04-09 22:20:46 +0900

    [refactor] change Sidebar nav items per user auth
    - per user auth(admin, manager, user), the displayed status is different depending on the user status.
    - add language

---
+++ src/@types/types.ts
@@ -0,0 +1,26 @@
+/**
+ * temporary folder for type storing
+ */
+
+export const UserAuth = {
+  Admin: 'Admin',
+  Manager: 'Manager',
+  User: 'User',
+} as const;
+export type UserAuth = typeof UserAuth[keyof typeof UserAuth];
+
+export const NavItem = {
+  TotalSWList: 'TotalSWList',
+  SubscribingSWList: 'SubscribingSWList',
+  SWDashboard: 'SWDashboard',
+
+  PJList: 'PJList',
+  LicenseList: 'LicenseList',
+
+  UserManagement: 'UserManagement',
+  SWManagement: 'SWManagement',
+
+  UserGuide: 'UserGuide',
+} as const;
+
+export type NavItem = typeof NavItem[keyof typeof NavItem];

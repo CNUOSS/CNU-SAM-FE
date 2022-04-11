76567f1b819ae24409492d2a51d26589ecee4117
parkeunsik <qkrdmstlr3@naver.com>
2022-04-12 00:08:36 +0900

    [feat] implement Table widget
    - implement Table widget for providing table ui
      - using react-virtualized
      - implement story file

---
+++ src/components/widgets/Table/Table.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Table, { AttributeType } from '.';
+
+export default {
+  title: 'Widget/Table',
+  component: Table,
+};
+
+const attributes: AttributeType[] = [
+  { label: '이름', dataKey: 'name', widthPercent: 20, disableSort: true },
+  { label: '설명', dataKey: 'description', widthPercent: 50, disableSort: false },
+];
+const items = [
+  { name: 'Brian Vaughn', description: 'Doftware engineer' },
+  { name: 'Arian Vaughn', description: 'Fftware engineer' },
+  { name: 'Crian Vaughn', description: 'Eoftware engineer' },
+];
+export const basicTable = () => (
+  <div style={{ height: '100%' }}>
+    <Table attributes={attributes} items={items} />
+  </div>
+);

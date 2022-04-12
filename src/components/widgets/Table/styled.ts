8c3bc64353a2063e4fc91debe3afe9e837c518cf
parkeunsik <qkrdmstlr3@naver.com>
2022-04-12 16:42:47 +0900

    [refactor] change Table header according to sortable
    - if header column is sortable, apply specific style

--- src/components/widgets/Table/styled.ts
+++ src/components/widgets/Table/styled.ts
@@ -1,11 +1,17 @@
 import styled from '@emotion/styled';
 
+interface HeaderItemProps {
+  sortable: boolean;
+}
+
 export const Container = styled.div`
   width: 100%;
   height: 100%;
   font-size: 1.8rem;
 
   .ReactVirtualized__Table__headerRow {
+    display: flex;
+    align-items: center;
     background-color: ${(props) => props.theme.colors.quaternary};
   }
 
@@ -14,14 +20,16 @@ export const Container = styled.div`
   }
 `;
 
-export const HeaderItem = styled.div`
+export const HeaderItem = styled.div<HeaderItemProps>`
   display: flex;
   gap: 1rem;
   justify-content: center;
+  cursor: ${(props) => (props.sortable ? 'pointer' : 'auto')};
   &:hover {
-    color: ${(props) => props.theme.colors.secondary};
+    color: ${(props) => props.sortable && props.theme.colors.secondary};
+
     path {
-      fill: ${(props) => props.theme.colors.secondary};
+      fill: ${(props) => props.sortable && props.theme.colors.secondary};
     }
   }
 `;

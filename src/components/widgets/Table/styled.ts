ded8c5fb70fe6742aacf71d84ddfb56917d246b5
parkeunsik <qkrdmstlr3@naver.com>
2022-04-14 00:01:40 +0900

    [feat] AddLectureSWTable add selfinput feature
    - for user self input, implement AddLectureSWTable self input

--- src/components/widgets/Table/styled.ts
+++ src/components/widgets/Table/styled.ts
@@ -15,6 +15,7 @@ export const Container = styled.div`
     background-color: ${(props) => props.theme.colors.quaternary};
   }
 
+  .ReactVirtualized__Grid__innerScrollContainer,
   .ReactVirtualized__Table__rowColumn,
   .ReactVirtualized__Table__row {
     overflow: visible !important;

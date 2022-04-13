82bf7b103156d7c297597a3a18c6f193d9f38277
parkeunsik <qkrdmstlr3@naver.com>
2022-04-13 15:24:40 +0900

    [feat] add icons
    - add icons
      - plug
      - trashcan

--- src/components/widgets/Icon/svg/index.ts
+++ src/components/widgets/Icon/svg/index.ts
@@ -2,5 +2,7 @@ export { default as asc } from './Asc';
 export { default as close } from './Close';
 export { default as desc } from './Desc';
 export { default as gear } from './Gear';
+export { default as plus } from './Plus';
 export { default as sort } from './Sort';
+export { default as trashcan } from './TrashCan';
 export { default as triangle } from './Triangle';

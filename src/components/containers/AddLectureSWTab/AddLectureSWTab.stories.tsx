695820d1c24838cab6fc7b91b45758c330a51e77
parkeunsik <qkrdmstlr3@naver.com>
2022-04-13 16:49:12 +0900

    [feat] implement AddLectureSWTab
    - To make new lecture SW information, implement AddLectureSWTab container

---
+++ src/components/containers/AddLectureSWTab/AddLectureSWTab.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AddLectureSWTab, { ItemType } from '.';
+import { generateAddLectureSWItem, generateStringArray } from '../../../__mocks__/create-mock';
+
+export default {
+  title: 'Container/AddLectureSWTab',
+  component: AddLectureSWTab,
+};
+
+const items: ItemType[] = [generateAddLectureSWItem(), generateAddLectureSWItem()];
+const companyList = generateStringArray(3);
+const productList = generateStringArray(3);
+
+export const addLectureSWTab = () => (
+  <AddLectureSWTab items={items} companyList={companyList} productList={productList} />
+);

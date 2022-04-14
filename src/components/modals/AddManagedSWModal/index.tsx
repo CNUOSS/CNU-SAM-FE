2231985c5ca69a9630e68f173fdef5ef0d9ddc14
parkeunsik <qkrdmstlr3@naver.com>
2022-04-14 14:23:11 +0900

    [refactor] use SelfDropdown widget in AddManagedSW modal
    - change Dropdown + Input using SelfDropdown widget

--- src/components/modals/AddManagedSWModal/index.tsx
+++ src/components/modals/AddManagedSWModal/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Dropdown from '../../../components/widgets/Dropdown';
+import SelfDropdown from '../../../components/widgets/SelfDropdown';
 import Input from '../../../components/widgets/Input';
 import Template from '../Template';
 import * as Style from './styled';
@@ -21,16 +21,12 @@ function AddManagedSWModal({
   onSubmit,
   closeModal,
 }: AddManagedSWModalProps) {
-  const companyList = [...defaultCompanyList, '직접 입력'];
   const [swName, setSWName] = useState(defaultSWName);
-  const [companyName, setCompanyName] = useState('');
-  const [selfCompanyName, setSelfCompanyName] = useState('');
-  const isSelfInput = companyName === '직접 입력';
+  const [company, setCompany] = useState('');
 
   const changeSWName = (event: React.ChangeEvent<HTMLInputElement>) => setSWName(event.target.value);
-  const changeSelfCompanyName = (event: React.ChangeEvent<HTMLInputElement>) => setSelfCompanyName(event.target.value);
-  const changeCompany = (selectedIndex: number) => setCompanyName(companyList[selectedIndex]);
-  const handleSubmit = () => onSubmit(isSelfInput ? companyName : selfCompanyName, swName);
+  const changeCompany = (company: string) => setCompany(company);
+  const handleSubmit = () => onSubmit(company, swName);
 
   return (
     <Template closeModal={closeModal}>
@@ -38,16 +34,15 @@ function AddManagedSWModal({
         <Style.Header>수업 용 SW 관리 항목에 추가하기</Style.Header>
         <Style.Description>당신의 행동으로 db의 운명이 달렸습니다. 알아서 책임지길 바라요.</Style.Description>
         <Style.InputWrapper>
-          <Style.CompanyWrapper>
-            <Dropdown
-              items={companyList}
-              label="SW 제조사"
-              currentIdx={defaultCompanyIndex}
-              width={isSelfInput ? '14rem' : '35rem'}
-              onClickItem={changeCompany}
-            />
-            {isSelfInput && <Input value={selfCompanyName} width="20rem" onChange={changeSelfCompanyName} />}
-          </Style.CompanyWrapper>
+          <SelfDropdown
+            items={defaultCompanyList}
+            label="SW 제조사"
+            currentIdx={defaultCompanyIndex}
+            width={35}
+            inputValue={company}
+            inputWidth={20}
+            onChange={changeCompany}
+          />
           <Input value={swName} label="SW 제품명" width="35rem" onChange={changeSWName} />
         </Style.InputWrapper>
         <Style.SubmitButton onClick={handleSubmit}>등록하기</Style.SubmitButton>

c3b7f1af978c26b82fd076882b42a531e0dcf63d
shellboy <qkrdmstlr3@naver.com>
2022-04-14 16:14:53 +0900

    Revert "[feat] add AddManagedSWModal to TotalLectureswListTab"
    This reverts commit 675546f6ee52464c015604a48ba545b6e340315d.

--- src/components/containers/TotalLectureSWListTab/index.tsx
+++ src/components/containers/TotalLectureSWListTab/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Input from '../../widgets/Input';
 import Dropdown from '../../widgets/Dropdown';
 import Table from '../../widgets/Table';
-import AddManagedSWModal from '../../modals/AddManagedSWModal';
 import { TotalLectureSWListAttr } from '../../../@types/types';
 import { totalLectureSWListAttr } from '../../../common/constants';
 import * as Style from './styled';
@@ -17,18 +16,18 @@ export type ItemType = {
   [key in TotalLectureSWListAttr]: string | boolean;
 };
 
-// FIXME: remove this
+// TODO: remove this
 interface TotalLectureSWListProps {
   items: ItemType[];
   isAdmin: boolean;
 }
 
 function TotalLectureSWListTab({ items, isAdmin }: TotalLectureSWListProps) {
-  const [selectedItem, setSelectedItem] = useState<ItemType>();
-  // FIXME: not this list, list fetched from server. here is not exist all company's information
-  const companyList = items.map((item) => item.productCompany as string);
-
-  const clickItem = (item: ItemType) => () => setSelectedItem(item);
+  const clickItem = (item: ItemType) => () => {
+    // TODO: open modal logic
+    // eslint-disable-next-line no-console
+    console.log(item);
+  };
 
   const getNewManaged = (managed: boolean | string, onClick: () => void) => {
     if (!managed) return 'No';
@@ -45,40 +44,26 @@ function TotalLectureSWListTab({ items, isAdmin }: TotalLectureSWListProps) {
     return { ...others, managed: newManaged };
   });
 
-  const closeModal = () => setSelectedItem(undefined);
-  const addNewManagedSW = () => {};
-
   return (
-    <>
-      {selectedItem && (
-        <AddManagedSWModal
-          defaultCompanyList={companyList}
-          defaultCompanyIndex={companyList.findIndex((company) => company === selectedItem.productCompany)}
-          defaultSWName={selectedItem.productName as string}
-          onSubmit={addNewManagedSW}
-          closeModal={closeModal}
-        />
-      )}
-      <Style.Container>
-        <Style.Description>Description</Style.Description>
-        <Style.Form>
-          <Style.InputList>
-            <Dropdown items={ORGANIZATION} label="조직분류" width="16rem" onClickItem={() => {}} />
-            <Dropdown items={YEARS} label="년도" width="10rem" onClickItem={() => {}} />
-            <Dropdown items={SEMESTER} label="학기" width="8rem" onClickItem={() => {}} />
-            <Dropdown items={DIVISION} label="이수구분" width="13rem" onClickItem={() => {}} />
-            <Input value="" label="과목이름" width="20rem" onChange={() => {}} />
-            <Input value="" label="과목번호" width="14rem" onChange={() => {}} />
-            <Input value="" label="등록자" width="14rem" onChange={() => {}} />
-          </Style.InputList>
-          <Style.InquireButton>조회하기</Style.InquireButton>
-        </Style.Form>
-        <Style.TableTitle>등록된 수업용 SW</Style.TableTitle>
-        <Style.TableWrapper>
-          <Table items={parsedItem} attributes={totalLectureSWListAttr} />
-        </Style.TableWrapper>
-      </Style.Container>
-    </>
+    <Style.Container>
+      <Style.Description>Description</Style.Description>
+      <Style.Form>
+        <Style.InputList>
+          <Dropdown items={ORGANIZATION} label="조직분류" width="16rem" onClickItem={() => {}} />
+          <Dropdown items={YEARS} label="년도" width="10rem" onClickItem={() => {}} />
+          <Dropdown items={SEMESTER} label="학기" width="8rem" onClickItem={() => {}} />
+          <Dropdown items={DIVISION} label="이수구분" width="13rem" onClickItem={() => {}} />
+          <Input value="" label="과목이름" width="20rem" onChange={() => {}} />
+          <Input value="" label="과목번호" width="14rem" onChange={() => {}} />
+          <Input value="" label="등록자" width="14rem" onChange={() => {}} />
+        </Style.InputList>
+        <Style.InquireButton>조회하기</Style.InquireButton>
+      </Style.Form>
+      <Style.TableTitle>등록된 수업용 SW</Style.TableTitle>
+      <Style.TableWrapper>
+        <Table items={parsedItem} attributes={totalLectureSWListAttr} />
+      </Style.TableWrapper>
+    </Style.Container>
   );
 }
 

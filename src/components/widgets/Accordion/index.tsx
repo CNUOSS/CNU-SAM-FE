a13314fe802807d013336f94561cd23b6ef79867
shellboy <qkrdmstlr3@naver.com>
2022-04-14 16:14:53 +0900

    Revert "[feat] add translation to accordion widget"
    This reverts commit bcf9d5a75b1ab3c1d7532fc586097e0930352409.

--- src/components/widgets/Accordion/index.tsx
+++ src/components/widgets/Accordion/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import { theme } from '../../../style/theme';
 import Icon from '../Icon';
 import * as Style from './styled';
@@ -12,7 +11,6 @@ interface AccordionProps {
 }
 
 function Accordion({ title, items, disable = false, onClickItem }: AccordionProps) {
-  const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleList = () => setIsOpen((prev) => !prev);
@@ -21,7 +19,7 @@ function Accordion({ title, items, disable = false, onClickItem }: AccordionProp
   return (
     <Style.Container>
       <Style.Category onClick={toggleList} disable={disable}>
-        {t(`page:${title}`)}
+        {title}
         <Style.IconWrapper isOpen={isOpen}>
           <Icon icon="triangle" color={disable ? 'white' : theme.colors.primary} />
         </Style.IconWrapper>
@@ -29,7 +27,7 @@ function Accordion({ title, items, disable = false, onClickItem }: AccordionProp
       <Style.List isOpen={isOpen} numOfItems={items.length}>
         {items.map((item) => (
           <Style.Item key={item} onClick={() => handleClickItem(item)} disable={disable}>
-            {t(`page:${item}`)}
+            {item}
           </Style.Item>
         ))}
       </Style.List>

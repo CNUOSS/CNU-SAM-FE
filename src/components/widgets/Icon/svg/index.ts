21bca8f0c3e56b3cfc37d9bb069bc81863bef665
shellboy <qkrdmstlr3@naver.com>
2022-04-14 16:14:53 +0900

    Revert "[feat] add sort icons"
    This reverts commit 69b04743f29870f0c2ddf03fce69ee869a64e123.

--- src/components/widgets/Icon/svg/index.ts
+++ src/components/widgets/Icon/svg/index.ts
@@ -1,6 +1,3 @@
-export { default as asc } from './Asc';
 export { default as close } from './Close';
-export { default as desc } from './Desc';
-export { default as gear } from './Gear';
-export { default as sort } from './Sort';
 export { default as triangle } from './Triangle';
+export { default as gear } from './Gear';

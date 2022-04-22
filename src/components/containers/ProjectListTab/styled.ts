53243cf3c4cb825ec56e9784f45acf6ef7d9a0f1
parkeunsik <qkrdmstlr3@naver.com>
2022-04-22 16:06:33 +0900

    [feat] implement ProjectListTab ui
    - For showing projects, implement ProjectListTab ui

---
+++ src/components/containers/ProjectListTab/styled.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const TableWrapper = styled.div`
+  margin-top: 3rem;
+  height: 50rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+`;

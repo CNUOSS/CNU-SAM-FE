4c1c589745b79c38f86394d40787b4d0c2f5def4
parkeunsik <qkrdmstlr3@naver.com>
2022-04-26 00:58:40 +0900

    [feat] implement LicenseListTab ui
    - for showing licenses, implement LicenseListTab ui
      - implement story file
      - implement test code

--- src/__mocks__/create-mock.ts
+++ src/__mocks__/create-mock.ts
@@ -3,6 +3,7 @@ import { ItemType as TotalLectureSWItemAttrType } from '../components/containers
 import { ItemType as AddLectureSWItemAttrType } from '../components/containers/AddOrUpdateLectureSWTab/Table';
 import { ItemType as LectureSWManagementAttrType } from '../components/containers/LectureSWManagementTab';
 import { ItemType as SubscribedSWAttrType } from '../components/containers/SubscribedSWTab';
+import { ItemType as LicenseAttrType } from '../components/containers/LicenseListTab';
 import { ItemType as ProjectAttrType } from '../components/containers/ProjectListTab';
 import { SummarizedVersionType } from '../components/containers/ProjectDetailTab';
 
@@ -87,4 +88,11 @@ export const generateProjectListItem = (): ProjectAttrType => ({
 export const generateVersionListItem = (): SummarizedVersionType => ({
   versionName: generateString(8),
   createdDt: generateString(8),
+});
+
+export const generateLicenseListItem = (): LicenseAttrType => ({
+  licenseName: generateString(8),
+  licenseType: generateString(8),
+  licenseUrl: generateString(20),
+  restriction: generateString(30),
 });

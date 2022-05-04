1839a5f7e49b12fff54a72daeb944024661ac78b
parkeunsik <qkrdmstlr3@naver.com>
2022-05-04 23:54:54 +0900

    [test] refactor LicenseListTab test code
    - use mocking tools (jest and storybook)

--- src/components/containers/LicenseListTab/LicenseListTab.stories.tsx
+++ src/components/containers/LicenseListTab/LicenseListTab.stories.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import withMock from 'storybook-addon-mock';
 import LicenseListTab from '.';
+import { getLicenseListAPI } from '../../../apis/license';
 import { generateLicenseListItem } from '../../../__mocks__/create-mock';
 
 export default {
   title: 'Container/LicenseListTab',
   component: LicenseListTab,
+  decorators: [withMock],
 };
 
-const items = [generateLicenseListItem(), generateLicenseListItem(), generateLicenseListItem()];
-export const tab = () => <LicenseListTab items={items} />;
+const Tab = (args: any) => <LicenseListTab {...args} />;
+
+export const Default = Tab.bind({});
+(Default as any).parameters = {
+  mockData: [
+    {
+      url: getLicenseListAPI,
+      method: 'GET',
+      status: 200,
+      response: [generateLicenseListItem(), generateLicenseListItem(), generateLicenseListItem()],
+    },
+  ],
+};
--- src/components/containers/LicenseListTab/LicenseListTab.test.tsx
+++ src/components/containers/LicenseListTab/LicenseListTab.test.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import LicenseListTab from '.';
-import { render, screen } from '../../../libs/rtl-utils';
-import { generateLicenseListItem } from '../../../__mocks__/create-mock';
+import { render, screen, waitFor } from '../../../libs/rtl-utils';
 import { licenseListAttr } from '../../../common/constants';
+import { generateLicenseListItem } from '../../../__mocks__/create-mock';
 
-const items = [generateLicenseListItem(), generateLicenseListItem(), generateLicenseListItem()];
-const renderApp = () => render(<LicenseListTab items={items} />);
+const renderApp = () => render(<LicenseListTab />);
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
 
 describe('Container/LicenseListTab', () => {
   describe('rendering test', () => {
-    it('if admin', () => {
+    it('if admin', async () => {
+      mockedAxios.get.mockResolvedValueOnce([
+        generateLicenseListItem(),
+        generateLicenseListItem(),
+        generateLicenseListItem(),
+      ]);
       renderApp();
 
-      const attrs = screen.getAllByTestId('table-attr').map((attr) => attr.textContent);
-      expect(attrs).toEqual(licenseListAttr.map((attr) => attr.label));
+      await waitFor(() => {
+        const attrs = screen.getAllByTestId('table-attr').map((attr) => attr.textContent);
+        expect(attrs).toEqual(licenseListAttr.map((attr) => attr.label));
+      });
     });
 
     // it('if not admin', () => {

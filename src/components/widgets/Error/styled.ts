5d4800f385631d66568211bc3a571d60e8c734e0
parkeunsik <qkrdmstlr3@naver.com>
2022-05-04 22:56:15 +0900

    [feat] implement Error Widget
    - implement Error Widget If rendering failed to show error message
      - implement storybook
      - implement test code

---
+++ src/components/widgets/Error/styled.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Description = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  font-size: 2rem;
+`;

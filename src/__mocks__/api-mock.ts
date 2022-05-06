9286a429e65ba8e93ab63c7d3be5329f084d49b5
parkeunsik <qkrdmstlr3@naver.com>
2022-05-07 01:49:12 +0900

    [feat] add id property to LicenseType
    - Id is needed

--- src/__mocks__/api-mock.ts
+++ src/__mocks__/api-mock.ts
@@ -1,29 +1,26 @@
 import { generateString } from './create-mock';
-import { GetLicenseListResponseClientType } from '../apis/license';
+import { LicenseType, GetLicenseListResponseClientType } from '../apis/license';
 
+/**
+ * data mock
+ */
+export const generateLicenseMock = (): LicenseType => ({
+  id: generateString(5),
+  licenseName: generateString(5),
+  licenseType: generateString(7),
+  licenseUrl: generateString(15),
+  restrictions: [generateString(6), generateString(6), generateString(6)],
+});
+
+/**
+ * response mock
+ */
 export const generateGetLicensesResponseMock = (): GetLicenseListResponseClientType => ({
   meta: {
     totalCount: 10,
     isEnd: false,
   },
-  licenses: [
-    {
-      licenseName: generateString(5),
-      licenseType: generateString(7),
-      licenseUrl: generateString(15),
-      restrictions: [generateString(6), generateString(6), generateString(6)],
-    },
-    {
-      licenseName: generateString(5),
-      licenseType: generateString(7),
-      licenseUrl: generateString(15),
-      restrictions: [generateString(6), generateString(6), generateString(6)],
-    },
-    {
-      licenseName: generateString(5),
-      licenseType: generateString(7),
-      licenseUrl: generateString(15),
-      restrictions: [generateString(6), generateString(6), generateString(6)],
-    },
-  ],
+  licenses: [generateLicenseMock(), generateLicenseMock(), generateLicenseMock()],
 });
+
+export const generateCreateLicenseResponseMock = (): LicenseType => generateLicenseMock();

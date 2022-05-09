4317ebe46b0bb2b1ada6c760ffb7b40e32508ddf
parkeunsik <qkrdmstlr3@naver.com>
2022-05-09 21:33:31 +0900

    [refactor] as api docs, change user api
    - as api docs, change user api
      - define type as api docs

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { SigninResponseServerType, signinResponseServer2Client, signinRequestClient2Server } from '../converter/user';
+import { RoleType } from '../converter/common';
 import { getToken } from '../utils/storage';
 
 export interface UserType {
   id: string;
-  role: string; // FIXME:
+  role: RoleType;
 }
 
 // Signin
@@ -12,16 +14,17 @@ export interface SigninRequestBodyClientType {
   password: string;
 }
 
-interface SigninResponseClientType {
-  jwt: string;
+export interface SigninResponseClientType {
   user: UserType;
+  accessToken: string;
+  uuid: string;
 }
 
 export const signinAPI = `/users/signin`;
 export const signinAPIFn = async (data: SigninRequestBodyClientType): Promise<SigninResponseClientType> => {
-  const result = await axios.post(signinAPI, { data });
-  if (result.data) return result.data;
-  return Promise.reject(result.data);
+  const response = await axios.post<SigninResponseServerType>(signinAPI, { data: signinRequestClient2Server(data) });
+  if (response.data) return signinResponseServer2Client(response.data);
+  return Promise.reject(response.data);
 };
 
 // Logout
@@ -30,7 +33,7 @@ export const logoutAPI = `/users/logout`;
 // refresh
 export const refreshAPI = `/users/self`;
 export const refreshAPIFn = async () => {
-  const result = await axios.get(refreshAPI, { headers: { Authorization: getToken() } });
-  if (result.data) return result.data;
-  return Promise.reject(result.data);
+  const response = await axios.get(refreshAPI, { headers: { Authorization: `Bearer ${getToken('at')}` } });
+  if (response.data) return response.data;
+  return Promise.reject(response.data);
 };

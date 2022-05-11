5b7e8b9d1d28c595bbe3aa4f6827a7c60c0e9eb8
parkeunsik <qkrdmstlr3@naver.com>
2022-05-11 21:40:09 +0900

    [test] add AuthProvider to test setting files
    - add AuthProvider to storybook preview.js
    - add AuthProvider to rtl
      - update customRenderer to receive props

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { AuthProvider } from '../src/libs/auth';
 import GlobalStyle from '../src/style/GlobalStyle';
 import { theme } from '../src/style/theme';
 import '../src/libs/i18n';
@@ -12,8 +13,10 @@ export const decorators = [
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
-          <Story />
-          <GlobalStyle />
+          <AuthProvider>
+            <Story />
+            <GlobalStyle />
+          </AuthProvider>
         </ThemeProvider>
       </RecoilRoot>
     </QueryClientProvider>

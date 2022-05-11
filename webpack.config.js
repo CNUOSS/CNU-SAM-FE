1a093642ef74d85f0121863400a506a351cd5065
parkeunsik <qkrdmstlr3@naver.com>
2022-05-11 21:49:48 +0900

    [feat] add dotenv to hide server url
    - install dotenv-webpack to hide server url

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -38,6 +39,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
     }),

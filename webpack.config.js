ca09cd3463e1dd8ff9f9f8f72e579b3de77dd0e7
parkeunsik <qkrdmstlr3@naver.com>
2022-05-13 02:14:04 +0900

    [chore] setting absolute path
    - install babel library
    - specify the path

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,21 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@@types': path.resolve(__dirname, './src/@types'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@converter': path.resolve(__dirname, './src/converter'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@languages': path.resolve(__dirname, './src/languages'),
+      '@libs': path.resolve(__dirname, './src/libs'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@recoil': path.resolve(__dirname, './src/recoil'),
+      '@style': path.resolve(__dirname, './src/style'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
   },
   devServer: {
     static: {

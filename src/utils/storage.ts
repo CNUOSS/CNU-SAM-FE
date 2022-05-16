9c1714b3fd7b3eb5af50acd65338b1ef71376b32
parkeunsik <qkrdmstlr3@naver.com>
2022-05-16 22:01:59 +0900

    [fix] fix storage getItem function error
    - if there is no item for entered key, error occur
    - before JSON.parse, check item exist

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,7 @@
 // TODO: MOVE NAME
-export const getToken = (key: string) => JSON.parse(window.localStorage.getItem(key) || '');
+export const getToken = (key: string) => {
+  const item = window.localStorage.getItem(key);
+  return item && JSON.parse(item);
+};
 export const setToken = (key: string, token: string) => window.localStorage.setItem(key, JSON.stringify(token));
 export const clearToken = (key: string) => window.localStorage.removeItem(key);

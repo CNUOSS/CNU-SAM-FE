1deb26f749e47708bde8ded04a01047d8b6791db
parkeunsik <qkrdmstlr3@naver.com>
2022-05-16 16:07:42 +0900

    [feat] implement LoadingModal modal
    - To show load state, implement LoadingModal
      - implement storybook
      - implement test code

---
+++ src/components/modals/LoadingModal/LoadingModal.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoadingModal from '.';
+
+export default {
+  title: 'Modal/LoadingModal',
+  component: LoadingModal,
+};
+
+export const loadingModal = () => <LoadingModal />;

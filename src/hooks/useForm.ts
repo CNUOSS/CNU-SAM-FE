cea44147dcbf9a09be70cb236060f91acd0ba790
parkeunsik <qkrdmstlr3@naver.com>
2022-05-17 22:05:25 +0900

    [refactor] change types of useForm.ts
    - apply state to Partial option

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,10 +3,6 @@ import { inOrOut } from '@utils/module';
 
 type ErrorType = 'required' | 'maxLength' | 'minLength';
 
-type StateType<T> = {
-  [key in keyof T]: string;
-};
-
 type ArrayStateType<T> = {
   [key in keyof T]: string[];
 };
@@ -27,20 +23,23 @@ type Validator<T> = {
 
 interface useFormType<T> {
   error: Error<T> | undefined;
-  change: (key: keyof T) => (event: React.ChangeEvent<HTMLInputElement> | { target: { value: string } }) => void;
+  change: (key: keyof T) => (event: React.ChangeEvent<HTMLInputElement> | string) => void;
   getValue: (key: keyof T) => string;
+  getAllValue: () => Partial<T>;
   getArrayValues: (key: keyof T) => string[];
   control: (name: keyof T, value: string) => void;
   handleSubmit: (fn: (data: T) => void) => (event?: any) => void;
 }
 
-function useForm<T>(validators?: Validator<T>): useFormType<T> {
-  const [state, setState] = useState<StateType<T>>({} as StateType<T>);
+function useForm<T extends object>(validators?: Validator<T>): useFormType<T> {
+  const [state, setState] = useState<Partial<T>>({});
   const [arrayState, setArrayState] = useState<ArrayStateType<T>>({} as ArrayStateType<T>);
   const [error, setError] = useState<Error<T>>();
 
-  const change = (key: keyof T) => (event: React.ChangeEvent<HTMLInputElement> | { target: { value: string } }) =>
-    setState((prev) => ({ ...prev, [key]: event.target.value }));
+  const change = (key: keyof T) => (event: React.ChangeEvent<HTMLInputElement> | string) => {
+    if (typeof event === 'string') setState((prev) => ({ ...prev, [key]: event }));
+    else setState((prev) => ({ ...prev, [key]: event.target.value }));
+  };
 
   const checkNotValidator = ([key, val]: any, result: any): boolean => {
     return val.some((v: ValidatorItemType) => {
@@ -66,9 +65,11 @@ function useForm<T>(validators?: Validator<T>): useFormType<T> {
     if (!notValidate) fn({ ...state, ...arrayState } as unknown as T);
   };
 
-  const getValue = (key: keyof T): string => state[key] || '';
+  const getValue = (key: keyof T): string => (state[key] || '') as string;
 
   const getArrayValues = (key: keyof T): string[] => arrayState[key];
+
+  const getAllValue = () => state;
 
   const control = (name: keyof T, value: string) => {
     setArrayState((prev) => ({
@@ -81,6 +82,7 @@ function useForm<T>(validators?: Validator<T>): useFormType<T> {
     error,
     change,
     getValue,
+    getAllValue,
     getArrayValues,
     control,
     handleSubmit,

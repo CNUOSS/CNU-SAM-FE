181b1a15d6f9f8238d03713c5617fbcb1cbe08ef
parkeunsik <qkrdmstlr3@naver.com>
2022-05-28 22:08:22 +0900

    [feat] implement getProjectDetail feature
    - when projectDetail is opened, get project info

--- src/components/containers/ProjectDetailTab/index.tsx
+++ src/components/containers/ProjectDetailTab/index.tsx
@@ -1,14 +1,26 @@
+// Dependencies
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import { tabState } from '@recoil/tab';
+
+// Components
 import TabTemplate from '@components/templates/TabTemplate';
 import DefaultText from '@components/widgets/DefaultText';
 import Button from '@components/widgets/Button';
 import Table from '@components/widgets/Table';
 import EnrollVersionTab from '@components/containers/EnrollVersionTab';
+import AsyncBoundaryWrapper from '../AsyncBoundaryWrapper';
+
+// APIs
+import { getProjectDetailAPI } from '@apis/project';
+import { getProjectDetailResponseServer2Client } from '@converter/project';
+
+import useFetch from '@hooks/useFetch';
 import { VersionListAttr } from '@@types/types';
 import { versionListAttr } from '@common/constants';
-import { tabState } from '@recoil/tab';
+import { GetProjectDetailResponseClientType } from '@@types/client';
 import { compareTabs } from '@utils/manage-tabs';
+import { useAuth } from '@libs/auth';
 import * as Style from './styled';
 
 export type SummarizedVersionType = {
@@ -23,40 +35,50 @@ interface ProjectDetailTabProps {
 
 function ProjectDetailTab({ projectId, versions = [] }: ProjectDetailTabProps) {
   const setTabState = useSetRecoilState(tabState);
+  const { user } = useAuth();
+  const { data } = useFetch<GetProjectDetailResponseClientType>(
+    getProjectDetailAPI.url(projectId),
+    {},
+    { suspense: false }, // TODO: how to process suspense clearly?
+    { response: getProjectDetailResponseServer2Client }
+  );
 
-  const parsedVersions = versions.map((version, index) => ({
-    ...version,
-    number: index + 1,
-    temp: <Button>라이선스 지킴이</Button>,
-  }));
+  const parsedVersions =
+    data?.versionList.map((version, index) => ({
+      ...version,
+      number: index + 1,
+      temp: <Button>라이선스 지킴이</Button>,
+    })) || [];
 
   const clickEnrollVersionButton = () => {
-    // TODO: if user, add authority compare logic
     setTabState((oldState) =>
       compareTabs(oldState, `프로젝트명: 버전등록`, <EnrollVersionTab projectName="프로젝트명" />)
     );
   };
 
+  if (!data) return <></>;
   return (
-    <TabTemplate description="Description" onUpdate={() => {}}>
-      <Style.BackGroundBox>
-        <Style.InputWrapper>
-          <DefaultText label="프로젝트명">프로젝트명</DefaultText>
-          <DefaultText label="소유자">소유자</DefaultText>
-          <DefaultText label="카테고리">카테고리</DefaultText>
-          <DefaultText label="라이선스">라이선스</DefaultText>
-        </Style.InputWrapper>
-        <Style.DescriptionWrapper>
-          <Style.Label>설명</Style.Label>
-          <Style.DescriptionInput>설명</Style.DescriptionInput>
-        </Style.DescriptionWrapper>
-      </Style.BackGroundBox>
-      <Style.TableWrapper>
-        <Style.EnrollButtonWrapper>
-          <Button onClick={clickEnrollVersionButton}>버전 등록하기</Button>
-        </Style.EnrollButtonWrapper>
-        <Table title="프로젝트 버전" attributes={versionListAttr} items={parsedVersions} />
-      </Style.TableWrapper>
+    <TabTemplate description="Description">
+      <AsyncBoundaryWrapper>
+        <Style.BackGroundBox>
+          <Style.InputWrapper>
+            <DefaultText label="프로젝트명">{data.projectName}</DefaultText>
+            <DefaultText label="소유자">{data.userId}</DefaultText>
+            <DefaultText label="카테고리">{data.projectCategoryName}</DefaultText>
+            <DefaultText label="라이선스">{data.ossLicenseName}</DefaultText>
+          </Style.InputWrapper>
+          <Style.DescriptionWrapper>
+            <Style.Label>설명</Style.Label>
+            <Style.DescriptionInput>{data.projectDescription}</Style.DescriptionInput>
+          </Style.DescriptionWrapper>
+        </Style.BackGroundBox>
+        <Style.TableWrapper>
+          <Style.EnrollButtonWrapper>
+            {user && user.id === data.userId && <Button onClick={clickEnrollVersionButton}>버전 등록하기</Button>}
+          </Style.EnrollButtonWrapper>
+          <Table title="프로젝트 버전" attributes={versionListAttr} items={parsedVersions} />
+        </Style.TableWrapper>
+      </AsyncBoundaryWrapper>
     </TabTemplate>
   );
 }

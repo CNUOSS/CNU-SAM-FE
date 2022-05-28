374350114b54e653075f7b2aa0a9fbf7189bbfb2
parkeunsik <qkrdmstlr3@naver.com>
2022-05-28 19:40:08 +0900

    [feat] implement create project api
    - to create project, implement create projects api

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,1 +1,5 @@
+import { MethodType } from '@hooks/useMutation';
+
 export const getProjectListAPI = `/projects/search?`;
+
+export const createProjectAPI: { method: MethodType; url: string } = { method: 'POST', url: '/projects' };

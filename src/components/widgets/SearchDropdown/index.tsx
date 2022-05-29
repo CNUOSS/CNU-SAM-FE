bb48b55a745a9ab1c5ccc0705a76f5da2e9ba695
parkeunsik <qkrdmstlr3@naver.com>
2022-05-29 15:17:44 +0900

    [feat] implement SearchDropdown Widget
    - When item is too many, use SearchDropdown

---
+++ src/components/widgets/SearchDropdown/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import * as Style from './styled';
+
+interface SearchDropdownProps {
+  items: string[];
+  error?: string;
+  label?: string;
+  width?: string;
+  isLoading?: boolean;
+  onChangeValue: (value: string) => void;
+  onClickSelected: () => void;
+}
+
+interface CoverProps {
+  children: React.ReactElement[];
+  label?: string;
+}
+
+const Cover = ({ children, label }: CoverProps) =>
+  label ? <Style.DropdownWrapper>{children}</Style.DropdownWrapper> : <>{children}</>;
+
+// TODO: apply debouncing
+function SearchDropdown({
+  items,
+  error,
+  label,
+  width = 'fit-content',
+  isLoading,
+  onChangeValue,
+  onClickSelected,
+}: SearchDropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState('');
+
+  const changeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
+  const closeList = () => setIsOpen(false);
+  const selectItem = (idx: number) => {
+    closeList();
+    setText(items[idx]);
+  };
+  const onFocus = () => {
+    if (!isOpen && onClickSelected) onClickSelected();
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    onChangeValue(text);
+  }, [text]);
+
+  return (
+    <Cover label={label}>
+      {label ? <Style.Label>{label}</Style.Label> : <></>}
+      <Style.Container width={width}>
+        <Style.CurrentItem value={text} onChange={changeInputValue} onFocus={onFocus} />
+        {isOpen && (
+          <Style.List>
+            {isLoading ? (
+              <>loading</>
+            ) : (
+              items.map((item, idx) =>
+                item.includes(text) ? (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <Style.Item key={`${item}${idx}`} onClick={() => selectItem(idx)}>
+                    {item}
+                  </Style.Item>
+                ) : (
+                  <></>
+                )
+              )
+            )}
+          </Style.List>
+        )}
+      </Style.Container>
+    </Cover>
+  );
+}
+
+export default SearchDropdown;

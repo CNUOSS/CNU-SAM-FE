e78c62833d810fa70161359a7df197127a68f20e
parkeunsik <qkrdmstlr3@naver.com>
2022-05-30 23:50:39 +0900

    [feat] search with manufacturer in RegistrationSWTab
    - admin can search with manufacturer in RegistrationSWTab

--- src/components/tabs/RegistrationSWTab/index.tsx
+++ src/components/tabs/RegistrationSWTab/index.tsx
@@ -3,12 +3,14 @@ import TabTemplate from '@components/templates/TabTemplate';
 import Table, { SearchInfoType } from './Table';
 import Input from '@components/widgets/Input';
 import TabForm from '@components/widgets/TabForm';
-import Dropdown from '@components/widgets/Dropdown';
+import DropdownContainer from '@components/containers/DropdownContainer';
 import AddManagedSWModal from '@components/modals/AddManagedSWModal';
 import AsyncBoundaryWrapper from '@components/containers/AsyncBoundaryWrapper';
 import * as Style from './styled';
 import { RegistrationSWType } from '@@types/client';
 import useForm from '@hooks/useForm';
+import { getManufacturersNamesAPI } from '@apis/data';
+import { getManufacturerNamesResponseServer2Client } from '@converter/data';
 
 function RegistrationSWTab() {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,6 +31,7 @@ function RegistrationSWTab() {
     toggleModal();
     setSelectedItem(item);
   };
+  const selectManufacturer = (manufacturer: string) => change('swMfr')(manufacturer);
   const handleSearch = () => setInfoStore((store) => ({ ...store, ...getAllValue() }));
 
   return (
@@ -47,7 +50,13 @@ function RegistrationSWTab() {
       <TabTemplate description="Description" onCreate={toggleModal}>
         <TabForm onSubmit={handleSearch} buttonText="조회하기">
           <Style.InputWrapper>
-            <Dropdown label="제조사" items={[]} width="21rem" onClickItem={() => {}} />
+            <DropdownContainer
+              label="제조사"
+              getUrl={getManufacturersNamesAPI}
+              width="21rem"
+              responseConverter={getManufacturerNamesResponseServer2Client}
+              onClickItem={selectManufacturer}
+            />
             <Input label="제품명" value={getValue('swName')} width="21rem" onChange={change('swName')} />
           </Style.InputWrapper>
         </TabForm>

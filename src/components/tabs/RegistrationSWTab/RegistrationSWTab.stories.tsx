1e43c91953b86148d73628c76df99fe255b36602
parkeunsik <qkrdmstlr3@naver.com>
2022-05-30 23:13:18 +0900

    [feat] implement get registrationswList feature
    - seperate Table Component from RegistrationSWTab Container

--- src/components/tabs/RegistrationSWTab/RegistrationSWTab.stories.tsx
+++ src/components/tabs/RegistrationSWTab/RegistrationSWTab.stories.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import RegistrationSWTab from '.';
-import { generateSWForLecture, generateStringArray } from '../../../__mocks__/create-mock';
 
 export default {
   title: 'Container/RegistrationSWTab',
   component: RegistrationSWTab,
 };
 
-const manufacturings = generateStringArray(3);
-const items = [generateSWForLecture(), generateSWForLecture(), generateSWForLecture()];
-export const registrationSWTab = () => <RegistrationSWTab items={items} manufacturings={manufacturings} />;
+export const registrationSWTab = () => <RegistrationSWTab />;

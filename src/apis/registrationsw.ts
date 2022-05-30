c72ea0440c68dd7d44d91591416b31953d62f145
parkeunsik <qkrdmstlr3@naver.com>
2022-05-30 22:32:24 +0900

    [feat] implement get registrationsw list api
    - implement api
    - define client/server type
    - implement converter

--- src/apis/registrationsw.ts
+++ src/apis/registrationsw.ts
@@ -1,3 +1,5 @@
 import { MethodType } from '@hooks/useMutation';
 
+export const getSubscribedSWAPI = `/registrations/search?`;
+
 export const createRegistrationSWAPI: { method: MethodType; url: string } = { method: 'POST', url: '/registrations' };

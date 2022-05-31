c873e492ec9eaebe12aff6d6a406c12c893d21b8
parkeunsik <qkrdmstlr3@naver.com>
2022-05-31 15:47:03 +0900

    [feat] implement update registrationsw api
    - implement api
    - implement converter
    - define client/server type

--- src/apis/registrationsw.ts
+++ src/apis/registrationsw.ts
@@ -4,9 +4,14 @@ export const getRegistrationSWListAPI = `/registrations/search?`;
 
 export const createRegistrationSWAPI: { method: MethodType; url: string } = { method: 'POST', url: '/registrations' };
 
-// FIXME: add dynamicUrl
 export const deleteRegistrationSWAPI: { method: MethodType; dynamicUrl: (id: number) => string; url: string } = {
   method: 'DELETE',
+  url: `/registrations/:id`,
+  dynamicUrl: (id: number) => `/registrations/${id}`,
+};
+
+export const updateRegistrationSWAPI: { method: MethodType; dynamicUrl: (id: number) => string; url: string } = {
+  method: 'PUT',
   url: `/registrations/:id`,
   dynamicUrl: (id: number) => `/registrations/${id}`,
 };

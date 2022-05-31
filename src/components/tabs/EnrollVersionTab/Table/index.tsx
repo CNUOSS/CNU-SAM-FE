9c765e02c5a89ca1d0f87b9277ab5ab536cf42f6
parkeunsik <qkrdmstlr3@naver.com>
2022-06-01 01:21:09 +0900

    [refactor] change Dropdown to SearchDropdown of EnrollVersionTab table
    - change dropdown

--- src/components/tabs/EnrollVersionTab/Table/index.tsx
+++ src/components/tabs/EnrollVersionTab/Table/index.tsx
@@ -2,9 +2,9 @@ import React, { useMemo, useState } from 'react';
 import Icon from '@components/widgets/Icon';
 import Input from '@components/widgets/Input';
 import TableWidget from '@components/widgets/Table';
-import Dropdown from '@components/widgets/Dropdown';
 import { OSSListAttr, Trash, Number } from '@@types/types';
 import { ossListAttr } from '@common/constants';
+import SearchDropdown from '@components/widgets/SearchDropdown';
 
 export type ItemType = {
   [key in OSSListAttr]: string;
@@ -31,7 +31,7 @@ function Table({ items, onAddNewItem, licenses, onDeleteItem }: TableProps) {
   const changeOssLocation = (event: React.ChangeEvent<HTMLInputElement>) => setOssLocation(event.target.value);
   const changeOssName = (event: React.ChangeEvent<HTMLInputElement>) => setOssName(event.target.value);
   const changeOssVersion = (event: React.ChangeEvent<HTMLInputElement>) => setOssVersion(event.target.value);
-  const changeLicense = (licenseIdx: number) => setLicense(licenses[licenseIdx]);
+  const changeLicense = (license: string) => setLicense(license);
   const changeOssUrl = (event: React.ChangeEvent<HTMLInputElement>) => setOssUrl(event.target.value);
   const addNewItem = () => {
     setOssLocation('');
@@ -57,7 +57,7 @@ function Table({ items, onAddNewItem, licenses, onDeleteItem }: TableProps) {
     ossLocation: <Input width="25rem" value={ossLocation} onChange={changeOssLocation} />,
     ossName: <Input width="15rem" value={ossName} onChange={changeOssName} />,
     ossVersion: <Input value={ossVersion} width="7rem" onChange={changeOssVersion} />,
-    license: <Dropdown width="20rem" items={[]} onClickItem={changeLicense} />,
+    license: <SearchDropdown items={licenses} width="20rem" onChangeValue={changeLicense} />,
     ossUrl: <Input width="20rem" value={ossUrl} onChange={changeOssUrl} />,
     trash: <></>,
   };

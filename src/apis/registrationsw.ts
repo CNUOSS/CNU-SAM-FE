928da88eeb91520fa97a3445ab1e37441828c757
parkeunsik <qkrdmstlr3@naver.com>
2022-05-31 15:29:03 +0900

    [feat] implement delete registration sw feature
    - implement delete api
    - implement delete registration sw feature in AddOrUpdateRegistrationSWModal

--- src/apis/registrationsw.ts
+++ src/apis/registrationsw.ts
@@ -3,3 +3,9 @@ import { MethodType } from '@hooks/useMutation';
 export const getRegistrationSWListAPI = `/registrations/search?`;
 
 export const createRegistrationSWAPI: { method: MethodType; url: string } = { method: 'POST', url: '/registrations' };
+
+// FIXME: add dynamicUrl
+export const deleteRegistrationSWAPI: { method: MethodType; url: (id: number) => string } = {
+  method: 'DELETE',
+  url: (id: number) => `/registrations/${id}`,
+};

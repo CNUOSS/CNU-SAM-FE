8ddfda357a13dad466dbc45b893decc7e2c0fed8
parkeunsik <qkrdmstlr3@naver.com>
2022-05-31 21:17:37 +0900

    [refactor] change onChange value
    - update event to string | ArrayBuffer | null

--- src/components/widgets/InputFile/index.tsx
+++ src/components/widgets/InputFile/index.tsx
@@ -3,16 +3,18 @@ import * as Style from './styled';
 
 interface InputFileProps {
   label: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: string | ArrayBuffer | null) => void;
 }
 
 function InputFile({ label, onChange }: InputFileProps) {
   const [filename, setFileName] = useState('');
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    reader.onload = () => onChange(reader.result);
     if (event.target.files?.length) {
       setFileName(event.target.files[0].name);
-      onChange(event);
+      reader.readAsBinaryString(event.target.files[0]);
     }
   };
 

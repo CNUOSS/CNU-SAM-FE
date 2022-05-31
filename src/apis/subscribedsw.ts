3e35e6bb59113c7e40455d941a69cdb9c75993c7
parkeunsik <qkrdmstlr3@naver.com>
2022-05-31 15:32:18 +0900

    [refactor] refactor delete apis
    - add dynamicUrl if delete url can be dynamic

--- src/apis/subscribedsw.ts
+++ src/apis/subscribedsw.ts
@@ -4,7 +4,8 @@ export const getSubscribedSWAPI = `/subscriptions/search?`;
 
 export const createSubscribedSWAPI: { method: MethodType; url: string } = { method: 'POST', url: '/subscriptions' };
 
-export const deleteSubscribedSWAPI: { method: MethodType; url: (id: number) => string } = {
+export const deleteSubscribedSWAPI: { method: MethodType; dynamicUrl: (id: number) => string; url: string } = {
   method: 'DELETE',
-  url: (id: number) => `/subscriptions/${id}`,
+  url: `subscriptions/:id`,
+  dynamicUrl: (id: number) => `/subscriptions/${id}`,
 };

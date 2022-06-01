2f579396e516dcd176974124e15d2e158f6ee647
parkeunsik <qkrdmstlr3@naver.com>
2022-06-01 17:45:12 +0900

    [feat] implement get Version feature
    - implement get Version feature

--- src/components/tabs/VersionDetailTab/index.tsx
+++ src/components/tabs/VersionDetailTab/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import TabTemplate from '@components/templates/TabTemplate';
 import Button from '@components/widgets/Button';
 import LineTable from '@components/widgets/LineTable';
-import { generateOssAnalysis } from '../../../__mocks__/api-mock';
 import { ossAnalysisListAttr } from '@common/constants';
 import * as Style from './styled';
+import { GetVersionDetailResponseClientType } from '@@types/client';
+import useFetch from '@hooks/useFetch';
+import { getVersionDetailAPI } from '@apis/version';
+import { getVersionDetailResponseServer2Client } from '@converter/version';
+import { getRestrictionsAPI } from '@apis/data';
+import { getRestrictionResponseServer2Client } from '@converter/data';
 
 interface VersionDetailTabProps {
   projectId: number;
@@ -12,14 +17,51 @@ interface VersionDetailTabProps {
 }
 
 function VersionDetailTab({ projectId, versionId }: VersionDetailTabProps) {
-  const dummyOSS = [generateOssAnalysis(), generateOssAnalysis(), generateOssAnalysis()];
+  const { data: ossData } = useFetch<GetVersionDetailResponseClientType>(
+    getVersionDetailAPI.dynamicUrl(projectId, versionId),
+    {},
+    { suspense: false },
+    { response: getVersionDetailResponseServer2Client }
+  );
+  const { data: restrictions } = useFetch<string[]>(
+    getRestrictionsAPI,
+    {},
+    { suspense: false },
+    { response: getRestrictionResponseServer2Client }
+  );
+
   const deleteVersion = () => {};
+
+  const restrictionsHeader = [
+    { label: '라이선스', dataKey: 'license' },
+    ...(restrictions?.map((res) => ({ label: res, dataKey: res })) || []),
+  ];
+  const restrictionData =
+    ossData?.analysisRestriction.map((license) => {
+      const restrictions = license.restriction.reduce((acc, res) => {
+        acc[res.restrictionName] = '⚫️';
+        return acc;
+      }, {} as any);
+      return {
+        license: license.licenseName,
+        ...restrictions,
+      };
+    }) || [];
 
   return (
     <TabTemplate description="버전 상세보기">
-      <Button onClick={deleteVersion}>삭제하기</Button>
+      <Style.ButtonWrapper>
+        <Button theme="warning" onClick={deleteVersion}>
+          삭제하기
+        </Button>
+      </Style.ButtonWrapper>
       <Style.TableWrapper>
-        <LineTable columns={ossAnalysisListAttr} data={dummyOSS} />
+        <Style.TableTitle>사용중인 Open Source Software</Style.TableTitle>
+        <LineTable columns={ossAnalysisListAttr} data={ossData?.ossAnalysis || []} />
+      </Style.TableWrapper>
+      <Style.TableWrapper>
+        <Style.TableTitle>사용중인 Open Source Software의 라이선스 제약조건</Style.TableTitle>
+        <LineTable columns={restrictionsHeader} data={restrictionData} />
       </Style.TableWrapper>
     </TabTemplate>
   );
--- src/components/tabs/VersionDetailTab/styled.ts
+++ src/components/tabs/VersionDetailTab/styled.ts
@@ -1,5 +1,18 @@
 import styled from '@emotion/styled';
 
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const TableTitle = styled.h2`
+  margin-bottom: 1rem;
+  font-size: 1.8rem;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.secondary};
+`;
+
 export const TableWrapper = styled.div`
-  margin-top: 3rem;
+  margin-bottom: 5rem;
 `;

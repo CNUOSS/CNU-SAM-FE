b13ffd01b7581ee4b88beb7886de24f0079f9ce0
parkeunsik <qkrdmstlr3@naver.com>
2022-06-02 23:30:26 +0900

    [feat] implement get lecture sw feature
    - seperate table of LectureSWList to use suspense

--- src/components/tabs/LectureSWListTab/LectureSWListTab.stories.tsx
+++ src/components/tabs/LectureSWListTab/LectureSWListTab.stories.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import TotalLectureSWListTab, { ItemType } from '.';
-import { generateTotalLectureSWItem } from '../../../__mocks__/create-mock';
+import TotalLectureSWListTab from '.';
 
 export default {
   title: 'Container/TotalLectureSWListTab',
   component: TotalLectureSWListTab,
 };
 
-const items: ItemType[] = [
-  generateTotalLectureSWItem({}),
-  generateTotalLectureSWItem({}),
-  generateTotalLectureSWItem({}),
-];
-
-export const adminTab = () => <TotalLectureSWListTab isAdmin items={items} />;
-export const userTab = () => <TotalLectureSWListTab isAdmin={false} items={items} />;
+export const adminTab = () => <TotalLectureSWListTab />;
+export const userTab = () => <TotalLectureSWListTab />;

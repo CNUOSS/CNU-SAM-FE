8e7639e7b83aea17b6c3bd5e2512cc4898946dd7
parkeunsik <qkrdmstlr3@naver.com>
2022-06-03 00:05:21 +0900

    [feat] implement create lecture sw feature
    - implement api
    - implement converter
    - define client/server types

--- src/apis/lecturesw.ts
+++ src/apis/lecturesw.ts
@@ -1,1 +1,8 @@
+import { MethodType } from '@hooks/useMutation';
+
 export const getLectureSWListAPI = `/lectures/search?`;
+
+export const createLectureSWAPI: { method: MethodType; url: string } = {
+  method: 'POST',
+  url: '/lectures',
+};

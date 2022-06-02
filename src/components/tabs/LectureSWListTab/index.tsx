dfc63f7ee0cf91c61b0a1a168348b1206b5354ae
parkeunsik <qkrdmstlr3@naver.com>
2022-06-02 23:53:23 +0900

    [feat] implement get license sw search feature
    - use Dropdown and string array constant

--- src/components/tabs/LectureSWListTab/index.tsx
+++ src/components/tabs/LectureSWListTab/index.tsx
@@ -1,24 +1,27 @@
+// Dependencies
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
-import TabTemplate from '@components/templates/TabTemplate';
-import Input from '@components/widgets/Input';
-import Dropdown from '@components/widgets/Dropdown';
-import TabForm from '@components/widgets/TabForm';
-import Table, { ItemType, SearchInfoType } from './Table';
-import AddOrUpdateRegistrationSWModal from '@components/modals/AddOrUpdateRegistrationSWModal';
-import AddOrUpdateLectureSWTab from '@components/tabs//AddOrUpdateLectureSWTab';
 import { tabState } from '@recoil/tab';
-import { compareTabs } from '@utils/manage-tabs';
-import * as Style from './styled';
-import useForm from '@hooks/useForm';
-import AsyncBoundaryWrapper from '@components/containers/AsyncBoundaryWrapper';
 
-// FIXME: remove
-const year = new Date().getFullYear();
-const ORGANIZATION = ['학부', '공과대학원'];
-const SEMESTER = ['1학기', '2학기', '계절학기'];
-const DIVISION = ['전공(핵심)', '교양(필수)'];
-const YEARS = [year - 1, year, year + 1].map((year) => String(year));
+// Components
+import Input from '@components/widgets/Input';
+import TabForm from '@components/widgets/TabForm';
+import Dropdown from '@components/widgets/Dropdown';
+import TabTemplate from '@components/templates/TabTemplate';
+import DropdownContainer from '@components/containers/DropdownContainer';
+import AsyncBoundaryWrapper from '@components/containers/AsyncBoundaryWrapper';
+import AddOrUpdateLectureSWTab from '@components/tabs//AddOrUpdateLectureSWTab';
+import AddOrUpdateRegistrationSWModal from '@components/modals/AddOrUpdateRegistrationSWModal';
+import Table, { ItemType, SearchInfoType } from './Table';
+
+// Apis
+import { getDepartmentsAPI, getLectureTypesAPI } from '@apis/data';
+import { getDepartmentResponseServer2Client, getLectureTypesResponseServer2Client } from '@converter/data';
+
+import useForm from '@hooks/useForm';
+import { compareTabs } from '@utils/manage-tabs';
+import { NOT_CHOOSED, SEMESTER, YEARS } from '@common/constants';
+import * as Style from './styled';
 
 function LectureSWListTab() {
   const { change, getValue, getAllValue } = useForm<SearchInfoType>();
@@ -46,6 +49,10 @@ function LectureSWListTab() {
   };
 
   const closeModal = () => setSelectedItem(undefined);
+  const changeDepartment = (department: string) => change('department')(department);
+  const changeYear = (yearIdx: number) => change('year')(yearIdx ? YEARS[yearIdx] : '');
+  const changeSemester = (semesterIdx: number) => change('semester')(semesterIdx ? SEMESTER[semesterIdx] : '');
+  const changeLectureType = (lectureType: string) => change('lectureType')(lectureType);
   const handleSearch = () => setInfoStore((store) => ({ ...store, ...getAllValue() }));
 
   return (
@@ -53,10 +60,22 @@ function LectureSWListTab() {
       {selectedItem && <AddOrUpdateRegistrationSWModal closeModal={closeModal} />}
       <TabTemplate description="Description">
         <TabForm onSubmit={handleSearch} buttonText="조회하기">
-          <Dropdown items={ORGANIZATION} label="조직분류" width="16rem" onClickItem={() => {}} />
-          <Dropdown items={YEARS} label="년도" width="10rem" onClickItem={() => {}} />
-          <Dropdown items={SEMESTER} label="학기" width="8rem" onClickItem={() => {}} />
-          <Dropdown items={DIVISION} label="이수구분" width="13rem" onClickItem={() => {}} />
+          <DropdownContainer
+            label="조직분류"
+            getUrl={getDepartmentsAPI}
+            width="16rem"
+            onClickItem={changeDepartment}
+            responseConverter={getDepartmentResponseServer2Client}
+          />
+          <Dropdown items={[NOT_CHOOSED, ...YEARS]} label="년도" width="10rem" onClickItem={changeYear} />
+          <Dropdown items={[NOT_CHOOSED, ...SEMESTER]} label="학기" width="10rem" onClickItem={changeSemester} />
+          <DropdownContainer
+            label="이수구분"
+            getUrl={getLectureTypesAPI}
+            width="13rem"
+            onClickItem={changeLectureType}
+            responseConverter={getLectureTypesResponseServer2Client}
+          />
           <Input value={getValue('lectureName')} label="과목이름" width="20rem" onChange={change('lectureName')} />
           <Input value={getValue('lectureNum')} label="과목번호" width="14rem" onChange={change('lectureNum')} />
           <Input value={getValue('owner')} label="등록자" width="14rem" onChange={change('owner')} />

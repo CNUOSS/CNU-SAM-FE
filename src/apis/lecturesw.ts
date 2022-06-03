5c9171a43d34f3fdeeeebabdeff5436295eb25b8
parkeunsik <qkrdmstlr3@naver.com>
2022-06-03 15:22:05 +0900

    [feat] implement lecture sw delete feature
    - implement delete api
    - implement lecture sw delete feature

--- src/apis/lecturesw.ts
+++ src/apis/lecturesw.ts
@@ -6,3 +6,9 @@ export const createLectureSWAPI: { method: MethodType; url: string } = {
   method: 'POST',
   url: '/lectures',
 };
+
+export const deleteLectureSWAPI: { method: MethodType; url: string; dynamicUrl: (id: number) => string } = {
+  method: 'DELETE',
+  url: '/lectures/:id',
+  dynamicUrl: (id: number) => `/lectures/${id}`,
+};
--- src/components/tabs/AddOrUpdateLectureSWTab/index.tsx
+++ src/components/tabs/AddOrUpdateLectureSWTab/index.tsx
@@ -16,21 +16,21 @@ import Table, { ItemType } from './Table';
 // Apis
 import { getDepartmentsAPI, getLectureTypesAPI } from '@apis/data';
 import { createLectureSWRequestClient2Server } from '@converter/lecturesw';
-import { createLectureSWAPI, getLectureSWListAPI } from '@apis/lecturesw';
+import { createLectureSWAPI, deleteLectureSWAPI, getLectureSWListAPI } from '@apis/lecturesw';
 import { getDepartmentResponseServer2Client, getLectureTypesResponseServer2Client } from '@converter/data';
 
 // hooks
 import useForm from '@hooks/useForm';
 import useMutation from '@hooks/useMutation';
 
-import { CreateLectureSWRequestClientType, LectureSWType } from '@@types/client';
+import { CreateLectureSWRequestClientType } from '@@types/client';
 import { NOT_CHOOSED, SEMESTER, YEARS } from '@common/constants';
 import { deleteTabs } from '@utils/manage-tabs';
 import { useAuth } from '@libs/auth';
 import * as Style from './styled';
 
 interface AddOrUpdateLectureSWTabProps {
-  lectureSW?: LectureSWType[];
+  lectureSWId?: number;
 }
 
 type FormType = Pick<
@@ -38,11 +38,11 @@ type FormType = Pick<
   'year' | 'semester' | 'lectureName' | 'department' | 'lectureNum' | 'lectureType'
 >;
 
-function AddOrUpdateLectureSWTab({ lectureSW }: AddOrUpdateLectureSWTabProps) {
+function AddOrUpdateLectureSWTab({ lectureSWId }: AddOrUpdateLectureSWTabProps) {
   const queryClient = useQueryClient();
   const { user } = useAuth();
   const setTabState = useSetRecoilState(tabState);
-  const [items, setItems] = useState<ItemType[]>(lectureSW || []);
+  const [items, setItems] = useState<ItemType[]>([]);
   const { change, getValue, error, handleSubmit } = useForm<FormType>({
     year: [{ error: 'required' }],
     semester: [{ error: 'required' }],
@@ -55,6 +55,10 @@ function AddOrUpdateLectureSWTab({ lectureSW }: AddOrUpdateLectureSWTabProps) {
     queryClient.invalidateQueries(getLectureSWListAPI);
     setTabState((prev) => deleteTabs(prev, `수업용 SW 등록`));
   };
+  const deleteMutationSuccess = async () => {
+    queryClient.invalidateQueries(getLectureSWListAPI);
+    setTabState((prev) => deleteTabs(prev, `${lectureSWId} . 강의 수정`));
+  };
   const { mutate: createMutate } = useMutation({
     url: createLectureSWAPI.url,
     method: createLectureSWAPI.method,
@@ -62,6 +66,11 @@ function AddOrUpdateLectureSWTab({ lectureSW }: AddOrUpdateLectureSWTabProps) {
     converter: {
       request: createLectureSWRequestClient2Server,
     },
+  });
+  const { mutate: deleteMutate } = useMutation({
+    url: deleteLectureSWAPI.url,
+    method: deleteLectureSWAPI.method,
+    onSuccess: deleteMutationSuccess,
   });
 
   const changeDepartment = (department: string) => change('department')(department);
@@ -76,6 +85,10 @@ function AddOrUpdateLectureSWTab({ lectureSW }: AddOrUpdateLectureSWTabProps) {
     if (!user) return;
     createMutate({ ...data, ownerId: user.id, sw: items });
   };
+  const deleteLectureSW = () => {
+    if (!lectureSWId || !user) return;
+    deleteMutate({ dynamicUrl: deleteLectureSWAPI.dynamicUrl(lectureSWId) });
+  };
 
   return (
     <TabTemplate description="Description">
@@ -83,13 +96,13 @@ function AddOrUpdateLectureSWTab({ lectureSW }: AddOrUpdateLectureSWTabProps) {
         <Style.FormTitle>수업 정보 작성</Style.FormTitle>
         {error && <Style.Error>모든 값을 입력해주십시오</Style.Error>}
         <Style.ButtonWrapper>
-          {!lectureSW && <Button onClick={handleSubmit(createLectureSW)}>등록하기</Button>}
-          {lectureSW && (
-            <Button theme="warning" onClick={() => {}}>
+          {!lectureSWId && <Button onClick={handleSubmit(createLectureSW)}>등록하기</Button>}
+          {lectureSWId && (
+            <Button theme="warning" onClick={deleteLectureSW}>
               삭제하기
             </Button>
           )}
-          {lectureSW && <Button onClick={() => {}}>수정하기</Button>}
+          {lectureSWId && <Button onClick={() => {}}>수정하기</Button>}
         </Style.ButtonWrapper>
       </Style.FormTitleWrapper>
       <TabForm>

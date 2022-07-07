6d80875d356f3fec3f1d80e22b674a7359649e56
shellboy <qkrdmstlr3@naver.com>
2022-07-07 21:41:01 +0900

    test: remove all test code
    - to implement test code from bottom, remove all test code

--- src/components/containers/Workspace/Workspace.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Workspace from '.';
-
-export default {
-  title: 'Container/Workspace',
-  component: Workspace,
-};
-
-export const workspace = () => <Workspace />;
--- src/pages/Home/Home.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomePage from '.';
-
-export default {
-  title: 'Page/HomePage',
-  component: HomePage,
-};
-
-export const homepage = () => <HomePage />;
